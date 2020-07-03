56ae3460de7afb923bd6d93a7c3af88c6722bac1
mslakshmi610 <67191330+mslakshmi610@users.noreply.github.com>
2020-07-03 12:42:54 +0530

    Add files via upload

---
+++ assignment9_mul_all_items.py
@@ -0,0 +1,7 @@
+def mul(i):
+	c=1
+	for x in i:
+		c*=x
+	return c
+list=[2,5,7,9]
+print(mul(list))
---
+++ assignment9_shuffle.py
@@ -0,0 +1,4 @@
+import random
+mylist = ["Vizag", "Hyderabad", "Banglore"]
+random.shuffle(mylist)
+print(mylist)
