8f1fb7c285fddaf37e1c9784ee8648b8365bc5eb
mslakshmi610 <67191330+mslakshmi610@users.noreply.github.com>
2020-07-03 11:47:52 +0530

    Add files via upload

---
+++ assignment 8_convert_string.py
@@ -0,0 +1,4 @@
+str1="I'm lakshmi"
+print(str1.split(' '))
+str1="I'm-lakshmi"
+print(str1.split('-'))
---
+++ assignment 8_delete_charecter_ from string.py
@@ -0,0 +1,8 @@
+def remove(string, i):
+	a=string[:i]
+	b=string[i+1:]
+	return a+b
+	if __name__=='__main__':
+		string="lakshmi"
+	i=3
+	print(remove(string,i))
